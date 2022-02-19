687877d2c6508af90f606dba6c0562cd45011c43
rakitenkogermans <rakitenko.germans@gmail.com>
2022-02-19 19:52:39 +0200

    change color for plot

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-// Pick canvas
+// Pick elements
 const canvas = document.querySelector('#canvas');
+const btns = document.querySelectorAll('input[name="color"]');
+// const btnsContainer = document.querySelector('.toggle');
+
 // Get context for API
 const ctx = canvas.getContext('2d');
+
+btns.forEach((elem) => {
+    elem.addEventListener("change", function(event) {
+        drawImg(event.target.value)
+    });
+});
 
 // Set canvas resolution
 canvas.width = 286;
@@ -13,7 +22,6 @@ img.src = 'img/olivia.png';
 
 // Layout for plot
 const layout = {
-    title: 'Green color surface plot',
     autosize: false,
     width: 500,
     height: 500,
@@ -26,23 +34,45 @@ const layout = {
     paper_bgcolor: '#f5f5f5'
 };
 
-// Array to store pixels (2d array) in green 0-255
+// Array to store pixels (2d array) in color 0-255
 let colorArray = [];
 
 // When our image was loaded
 img.addEventListener('load', () => {
+    drawImg('green');
+});
+
+function drawImg(color) {
+    // btnsContainer.classList.add('disabled');
     // Draw image on canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(img, 0, 0);
     // Get array data from starting coords and end coords
     const pixelScan = ctx.getImageData(0, 0, canvas.width, canvas.height);
     const scannedData = pixelScan.data;
 
-    // Loop through array and taking only green color
+    colorArray = [];
+    // Loop through array and taking only one color
     for (let i = 0; i < scannedData.length; i += 4) {
-        colorArray.push(scannedData[i+1]);
-        scannedData[i] = 0;
-        scannedData[i+1] = scannedData[i+1];
-        scannedData[i+2] = 0;
+        if (color === 'red') {
+            colorArray.push(scannedData[i]);
+            scannedData[i] = scannedData[i];
+            scannedData[i+1] = 0;
+            scannedData[i+2] = 0;
+        }
+        if (color === 'green') {
+            colorArray.push(scannedData[i+1]);
+            scannedData[i] = 0;
+            scannedData[i+1] = scannedData[i+1];
+            scannedData[i+2] = 0;
+        }
+        if (color === 'blue') {
+            colorArray.push(scannedData[i+2]);
+            scannedData[i] = 0;
+            scannedData[i+1] = 0;
+            scannedData[i+2] = scannedData[i+2];
+        }
+
     }
 
     // Convert array to necessary format for plot
@@ -55,28 +85,41 @@ img.addEventListener('load', () => {
     // Put new image data
     ctx.putImageData(pixelScan, 0, 0);
 
-    createPlot();
-})
+    createPlot(color);
+    // btnsContainer.classList.remove('disabled');
+}
 
-function createPlot() {
+function createPlot(color) {
+    const colorscale = [
+        ['0.0', 'rgb(0,0,0)']
+    ];
+
+    if (color === 'red') {
+        colorscale.push(['1.0', 'rgb(255,0,0)'])
+    }
+    if (color === 'green') {
+        colorscale.push(['1.0', 'rgb(0,255,0)']);
+    }
+    if (color === 'blue') {
+        colorscale.push(['1.0', 'rgb(0,0,255,)']);
+    }
+
     const dataSurface = [{
         z: colorArray,
-        colorscale: [
-            ['0.0', 'rgb(0,0,0)'],
-            ['1.0', 'rgb(0,255,0)']
-        ],
+        colorscale,
         type: 'surface'
     }];
 
     const dataHeatmap = [{
         z: [...colorArray].reverse(),
-        colorscale: [
-            ['0.0', 'rgb(0,0,0)'],
-            ['1.0', 'rgb(0,255,0)']
-        ],
+        colorscale,
         type: 'heatmap'
     }];
 
-    Plotly.newPlot('heatmapPlot', dataHeatmap, {...layout, title: 'Green color plot', height: 700});
-    Plotly.newPlot('surfacePlot', dataSurface, layout);
+    Plotly.newPlot('heatmapPlot', dataHeatmap, {...layout, title: `${capitalize(color)} color plot`, height: 700});
+    Plotly.newPlot('surfacePlot', dataSurface, {...layout, title: `${capitalize(color)} color surface plot`});
+}
+
+function capitalize([letter, ...word]) {
+    return letter.toUpperCase() + word.join('');
 }
